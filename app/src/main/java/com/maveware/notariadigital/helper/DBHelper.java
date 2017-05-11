574aa78582a1c988e110d8cddae599fa4e313667
sckot <moy_ninho_10@hotmail.com>
2017-05-11 01:51:03 -0500

    Fix de cálculo de pagos. Fix look & feel de cotización.

--- app/src/main/java/com/maveware/notariadigital/helper/DBHelper.java
+++ app/src/main/java/com/maveware/notariadigital/helper/DBHelper.java
@@ -1,5 +1,6 @@
 package com.maveware.notariadigital.helper;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,24 +12,34 @@ import com.maveware.notariadigital.model.Persona;
  * Created by mgonzalezy on 13/04/2017.
  */
 
-public class DBHelper extends SQLiteOpenHelper {
+public abstract class DBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Notaria.db";
 
     private static final String TYPE_TEXT = "TEXT";
     private static final String COMMA_SEP = ",";
 
+    public String CREATE_QUERY;
+    public String DROP_QUERY;
+
 
     public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL(PersonaTable.CREATE_QUERY);
+    public DBHelper(Context context, String create_query){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        CREATE_QUERY = create_query;
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(getCREATE_QUERY());
+    }
+
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL(PersonaTable.DROP_QUERY);
+        db.execSQL(getDROP_QUERY());
         onCreate(db);
     }
 
@@ -36,4 +47,25 @@ public class DBHelper extends SQLiteOpenHelper {
         onUpgrade(db, oldVersion, newVersion);
     }
 
+    public void guardar(String table, ContentValues valores){
+        this.getWritableDatabase();
+        this.getWritableDatabase().insert(table, null, valores);
+        this.close();
+    }
+
+    public String getCREATE_QUERY() {
+        return CREATE_QUERY;
+    }
+
+    public void setCREATE_QUERY(String CREATE_QUERY) {
+        this.CREATE_QUERY = CREATE_QUERY;
+    }
+
+    public String getDROP_QUERY() {
+        return DROP_QUERY;
+    }
+
+    public void setDROP_QUERY(String DROP_QUERY) {
+        this.DROP_QUERY = DROP_QUERY;
+    }
 }
